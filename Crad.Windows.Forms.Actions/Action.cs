b6853d0800e498cbac278699c43260d88faf2853
Lex Li <support@lextm.com>
2012-09-11 20:33:54 +0800

    Action.Text does not need multi-line support. Removed that.

--- Crad.Windows.Forms.Actions/Action.cs
+++ Crad.Windows.Forms.Actions/Action.cs
@@ -115,7 +115,7 @@ namespace Crad.Windows.Forms.Actions
 
         #region common properties
         private string _text;
-        [Editor("System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(System.Drawing.Design.UITypeEditor)), DefaultValue(""), UpdatableProperty(), Localizable(true)]
+        [DefaultValue(""), UpdatableProperty(), Localizable(true)]
         public string Text
         {
             get { return _text; }
